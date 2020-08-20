ced17ebfeaf36924a0c5c6fc6ad7441dc1f17312
WSZ <WSZ@DESKTOP-0KB6F8B>
2020-08-20 16:50:31 +0800

    项目初始化

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/e421083458/gin_scaffold/router"
+	"github.com/e421083458/golang_common/lib"
+)
+
+func main() {
+	lib.InitModule("./conf/dev/", []string{"base", "mysql", "redis"})
+	defer lib.Destroy()
+
+	router.HttpServerRun()
+
+	quit := make(chan os.Signal)
+	signal.Notify(quit, syscall.SIGKILL, syscall.SIGQUIT, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	fmt.Println("123")
+	router.HttpServerStop()
+}
